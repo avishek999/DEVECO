feb117d2702ba4ea6a289e7b69c51edc23e1277c
avishek999 <avishekprasad0999@gmail.com>
2025-09-20 22:49:02 +0530

    img not owrking

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-import { generateTextAndImage } from "./service/geminiService.js";
+// import { generateTextAndImage } from "./service/geminiService.js";
+
+import { getImage } from "./service/geminiService.js";
 
 
-generateTextAndImage([{ msg: "write hello world in java , learn basic " }], ["what is jabva , java is programming language"]);
+// generateTextAndImage([{ msg: "write hello world in java , learn basic " }], ["what is jabva , java is programming language"]);
+
+
+
+getImage() 
--- service/geminiService.js
+++ service/geminiService.js
@@ -46,10 +46,9 @@ Context:
   });
 
   // The image is usually base64 inline data inside candidates
-  const imagePart =
-    imgResult.response.candidates?.[0]?.content?.parts?.find(
-      (p) => p.inlineData?.mimeType?.startsWith("image/")
-    );
+  const imagePart = imgResult.response.candidates?.[0]?.content?.parts?.find(
+    (p) => p.inlineData?.mimeType?.startsWith("image/")
+  );
 
   if (imagePart) {
     fs.writeFileSync(
@@ -61,3 +60,27 @@ Context:
     console.log("⚠️ No image returned by model");
   }
 }
+
+import { GoogleGenAI, Modality } from "@google/genai";
+
+const ai = new GoogleGenAI({apiKey:GEMINI_API});
+
+const prompt =
+  "Create a picture of a nano banana dish in a fancy restaurant with a Gemini theme";
+
+export const getImage = async () => {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash-image-preview",
+    contents: prompt,
+  });
+  for (const part of response.candidates[0].content.parts) {
+    if (part.text) {
+      console.log(part.text);
+    } else if (part.inlineData) {
+      const imageData = part.inlineData.data;
+      const buffer = Buffer.from(imageData, "base64");
+      fs.writeFileSync("gemini-native-image.png", buffer);
+      console.log("Image saved as gemini-native-image.png");
+    }
+  }
+};
